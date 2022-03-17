3d870ef97b9b6cc404d069df7cc2f995cb7e4614
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-03-17 19:46:29 -0300

    Create Ex_Salario_Mensal.py

---
+++ Ex_Salario_Mensal.py
@@ -0,0 +1,5 @@
+dinheiroDia = float(input("DIGITE SEU SALARIO POR DIA: "))
+dias = int(input("DIGITE O NUMERO DE DIAS TRABALHADOS: "))
+
+dinheiro = (dinheiroDia * dias) * 0.92
+print(f"Você irá receber {dinheiro} líquidos")
